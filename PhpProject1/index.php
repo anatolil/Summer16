a4e02e4de414719640ab3bd8ed2160ad90d46d65
toli <toli@10.112.192.223>
2016-08-10 13:43:57 +0300

    form and user auth

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -10,27 +10,51 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <form id="loginform" action="index.php">
-            <input type="text"><br>
-            <input type="password">
+        <form id="loginform" action="index.php" method="POST">
+            <input type="text" name="user"><br>
+            <input type="password" name="password">
             <input type="submit" value="Login">
         </form>
         <?php
+        
+        // get the login info from form
+        $uname = $_POST[user];
+        $upass = $_POST[password];
+        
+        //connection for db
         require_once ('config.php');
         require_once ('dbopen.php');
         
+        // check for user auth
+        $query = "SELECT * FROM MovieUsers WHERE vcUserName = '" . $uname . "' AND vcUserPassword = '" . $upass . "';";
+        $reults = mysql_query($query)
+                or die("You suck at authenticating users" . mysql_error());
+        
+        if(mysql_fetch_array($reults)) {
+            // user auth correct
+            echo "<script>alert('User auth');</script>";
+        } else {
+            // user auth not correct
+            echo "<script>alert('fuck off');</script>";
+        }
+        
+        // check for cookie
         if(!isset($_COOKIE[$movie_cookie])) {
             // cookie not set
         } else {
             // cookie set and user propably logged
         }
         
+        
+        // query all the movies in db
         $query = "SELECT * FROM Movie";
         $results = mysql_query($query)
                 or die("You suck at SQL " . mysql_error());
         
         $i = 0;                                                                 // number of movies
         
+        
+        // display all the movies on a page
         while ($row = mysql_fetch_array($results)) {
             echo "<div id=movie" . $i . " class='moviethumb'>";
             echo "<p class=movieName>" . $row[1] .  "</p>";                     // print movie name   consider using span
