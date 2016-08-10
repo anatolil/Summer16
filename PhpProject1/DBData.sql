72ca5159cb11c990d076fd538d5a8bb43f6ef3df
toli <toli@10.112.192.223>
2016-08-10 15:04:10 +0300

    correct data

--- PhpProject1/DBData.sql
+++ PhpProject1/DBData.sql
@@ -1,11 +1,7 @@
-CREATE TABLE MovieReviews (
-  idReview INT AUTO_INCREMENT,
-  vcText VARCHAR(250),
-  fkUser INT NOT NULL,
-  fkMovie INT NOT NULL,
+INSERT INTO MovieUsers(vcUserName, vcUserPassword, iLevel) VALUES
+  ('admin', 'admin', 1),
+  ('toli', 'salasana', 2);
 
-  PRIMARY KEY (idReview),
-  FOREIGN KEY (fkUser) REFERENCES MovieUsers(idUser),
-  FOREIGN KEY (fkMovie) REFERENCES Movie(idMovie)
-);
-
+INSERT INTO Movie(vcMovieName, vcImgSource, vcYTLink) VALUES
+  ('Terminator 2', 'terminator.jpg', 'https://www.youtube.com/watch?v=nxr2SV5znwI'),
+  ('Ghostbusters', NULL, 'https://www.youtube.com/watch?v=SM-l_3RaYsk');
