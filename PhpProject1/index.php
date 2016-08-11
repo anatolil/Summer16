ce7705e4c16bf79d6b8276aa179cbb1881c1f39e
toli <toli@10.112.192.223>
2016-08-11 17:57:30 +0300

    some logic changes

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -65,9 +65,9 @@ and open the template in the editor.
         
         // display all the movies on a page
         echo "<div class='movierow'>";                                          // row of 5 movies thumbs
-        while ($row = mysql_fetch_array($results) OR $i < 5) {
+        while ($row = mysql_fetch_array($results) AND $i < 5) {
             echo "<span id=movie" . $i . " class='moviethumb'>";                // div might also bee a good idea
-            //echo "<p class=movieName>" . $row[1] .  "</p>";                     // print movie name   consider using span
+            echo "<p class=movieName>" . $row[1] .  "</p>";                     // print movie name   consider using span
             
             if(is_null($row[2])) {
                 echo "<img class=movieImg src='pictures/noimage.jpg'></img>";
